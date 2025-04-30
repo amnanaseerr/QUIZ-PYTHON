bd0f104e0b5e748ed174b04f7d393fc163f0db3a
amna naseer <162809873+amnanaseerr@users.noreply.github.com>
2025-04-30 14:10:44 +0500

    Add files via upload

---
+++ quiz_game.py
@@ -0,0 +1,46 @@
+print("Welcome to my computer quiz!")
+playing = input("Do you want to play? ")
+if playing != "yes":
+    print("Good Bye!")
+    quit()
+
+print("okay! let's play :)")
+score = 0
+
+answer = input("What does CPU stands for? ")
+if answer.lower() == "central processing unit":
+    print("Correct!")
+    score +=1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stands for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score +=1
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stands for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score +=1
+else:
+    print("Incorrect!")
+
+answer = input("What does ROM stands for? ")
+if answer.lower() == "read only memory":
+    print("Correct!")
+    score +=1
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stands for? ")
+if answer.lower() == "power supply unit":
+    print("Correct!")
+    score +=1
+else:
+    print("Incorrect!")
+
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score/5)*100) + "%.")
